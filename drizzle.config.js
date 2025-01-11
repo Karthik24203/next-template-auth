494afa927397944a3702cefa25d49ac63658cc6a
Karthik V <karthikv24203@gmail.com>
2025-01-11 23:05:13 +0530

    congfig

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,9 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 config({ path: ".env.local" });
+
 export default defineConfig({
+  out: "./drizzle",
   schema: "./config/schema.js",
   dialect: "postgresql",
   dbCredentials: {
